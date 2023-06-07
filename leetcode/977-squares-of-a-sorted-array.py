693615df1dadb445bd645f30ab70248a35795e9e
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-07 02:35:17 -0300

    optimizing 977 - squares of a sorted array

--- leetcode/977-squares-of-a-sorted-array.py
+++ leetcode/977-squares-of-a-sorted-array.py
@@ -1,16 +1,18 @@
 def sortedSquares(nums):
-  s = 0
-  e = len(nums) - 1
-  squaredArr = []
-  while s <= e:
-    if nums[s] * nums[s] > nums[e] * nums[e]:
-      squaredArr.append(nums[s] * nums[s])
-      s += 1
+  l = 0
+  r = k = len(nums) - 1
+  ans = [0] * (k + 1)
+
+  while r >= l:
+    if nums[l] * nums[l] > nums[r] * nums[r]:
+      ans[k] = (nums[l] * nums[l])
+      l += 1
     else:
-      squaredArr.append(nums[e] * nums[e])
-      e -= 1
-  squaredArr.reverse()
-  return squaredArr #squaredArr[::-1]
+      ans[k] = (nums[r] * nums[r])
+      r -= 1
+    k -= 1
+
+  return ans #ans[::-1]
 
 print(sortedSquares([-4, -1, 0, 3, 10]))
 print(sortedSquares([-7, -3, 2, 3, 11]))
