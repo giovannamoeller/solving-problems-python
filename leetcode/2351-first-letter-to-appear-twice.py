95c4455ed00832b047865cda6478ae6098ca1a3e
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-13 13:01:35 -0300

    first letter to appear twice - improvements

--- leetcode/2351-first-letter-to-appear-twice.py
+++ leetcode/2351-first-letter-to-appear-twice.py
@@ -1,9 +1,9 @@
 def repeatedCharacter(s):
   charSet = set()
 
-  for i in range(len(s)):
-    if s[i] in charSet: return s[i]
-    charSet.add(s[i])
+  for c in s:
+    if c in charSet: return c
+    charSet.add(c)
 
 print(repeatedCharacter('abccbaacz'))
 print(repeatedCharacter('abcdd'))
